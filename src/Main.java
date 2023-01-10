50b5801ea3a25f845a82e3fbb737e82a3454abeb
Mayank Bhatt <bamayank854@gmail.com>
2023-01-10 12:21:20 +0530

    Multiplication function added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int a=10,b=12;
         int c=a+b;
+        int d=a*b;
         System.out.println(c);
+        System.out.println(d);
     }
 }
